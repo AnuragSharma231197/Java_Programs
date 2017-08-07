80359014f3f0046d3f14d00610fdd3aca179179a
AnuragSharma231197 <sharma.shanu012@gmail.com>
2017-08-06 17:55:21 -0700

    Swap

---
+++ swap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class swap{
+	public static void main(String[]args){
+		Scanner scan=new Scanner(System.in);
+		int num1=scan.nextInt();
+		int num2=scan.nextInt();
+		int num3=0;
+		 num3=num1;
+		 num1=num2;
+		 num2=num3;
+		System.out.println(num1);
+		System.out.println(num2);		}
+}
