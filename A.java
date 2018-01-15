69446e9a96ea0ace12cc3102d770bb5a6b19ccd0
AnuragSharma231197 <sharma.shanu012@gmail.com>
2018-01-15 23:06:08 +0530

    Random

---
+++ A.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class A{
+public static void main(String []args){
+	for(int j=1;j<10000;j++){
+		for(int i=1;i<20;i++){
+			if(j%i==0){
+				System.out.println(j);
+			}
+				}
+			}
+		}
+	}
